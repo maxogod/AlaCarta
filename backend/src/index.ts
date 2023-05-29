78963ad6dc244215e88c6ba1865d0ecf45182a36
maxogod <maxi18wars@gmail.com>
2023-05-29 20:40:13 -0300

    init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 const app = express()
-const PORT = '3000'
+dotenv.config()
+const PORT = process.env.DEV_PORT || 8000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')

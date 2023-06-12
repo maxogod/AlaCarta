a19190cf32661917f81696198432e572d7947e18
santi <santisevitz@hotmail.com>
2023-06-12 18:04:45 -0300

    custom colors :D

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      customOrange: "#F26B3F",
+      customBeige: "#EFE5CE",
+      customPink: "#F3AA97",
+      customRed: "#CE5160",
+      customYellow: "#FDBE5D",
+      customLightBlue: "#48C4D3",
+    },
     extend: {},
   },
   plugins: [],

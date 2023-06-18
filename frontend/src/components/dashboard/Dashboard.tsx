a4fe6628651fe4c797c7615930c62b5cf9ab6f73
santi <santisevitz@hotmail.com>
2023-06-17 22:21:51 -0300

    mejoras a catalogo, comienzo de mejoras a estadisticas

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import Draggable from 'react-draggable';
 import { NavBar } from './Header';
 import { Blurhash } from 'react-blurhash';
 
 
 interface category {
-    categoria: string
+    title: string
 }
 
 
-interface products {
+interface product {
     name: string
     price: number
     sales: number
@@ -16,7 +17,6 @@ interface products {
     img: string
     categories: category[]
     isAvailable: boolean
-
 }
 
 
@@ -24,6 +24,9 @@ const Dashboard = () => {
 
     const src = "https://toohotel.com/wp-content/uploads/2022/09/TOO_restaurant_Panoramique_vue_Paris_Seine_Tour_Eiffel_2.jpg"
     const [imageLoader, setImageLoader] = useState(false)
+
+    const [selectedProduct, setSelectedProduct] = useState<product | null>(null);
+
 
     useEffect(() => {
         const img = new Image()
@@ -33,10 +36,20 @@ const Dashboard = () => {
         img.src = src
     }, [src])
 
+    const handleProductClick = (product: product) => {
+        setSelectedProduct(product);
+    }
 
-    const dummyProducts: products[] = [
-        { name: "empanadas de jamon y queso", price: 500, sales: 850, description: "empanadas de jamon y queso, riqui riqui", img: "https://www.cucinare.tv/wp-content/uploads/2021/09/Empanadas-fritas-o-al-horno.jpg", categories: [], isAvailable: true },
-        { name: "pizza de muzza", price: 1600, sales: 48, description: "piza de muza, muy rica bro", img: "https://www.cucinare.tv/wp-content/uploads/2021/09/Empanadas-fritas-o-al-horno.jpg", categories: [], isAvailable: true },
+    const categories: category[] = [
+        { title: "vegano" },
+        { title: "picante" },
+        { title: "empanada" },
+        { title: "sin tacc" }
+    ]
+
+    const dummyProducts: product[] = [
+        { name: "empanadas de jamon y queso reeeeeeeeeeee largaaaaaaaaa", price: 500, sales: 850, description: "empanadas de jamon y queso, riqui riqui", img: "https://www.cucinare.tv/wp-content/uploads/2021/09/Empanadas-fritas-o-al-horno.jpg", categories: categories, isAvailable: true },
+        { name: "pizza de muzza", price: 1600, sales: 48, description: "piza de muza, muy rica bro aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", img: "https://www.cucinare.tv/wp-content/uploads/2021/09/Empanadas-fritas-o-al-horno.jpg", categories: [], isAvailable: true },
         { name: "hamburgesa con queso", price: 2500, sales: 55, description: "paty con queso bro", img: "https://www.cucinare.tv/wp-content/uploads/2021/09/Empanadas-fritas-o-al-horno.jpg", categories: [], isAvailable: true },
         { name: "fideos con solo tuco", price: 1500, sales: 5, description: "fideos con SOLO TUCO >:(", img: "https://www.cucinare.tv/wp-content/uploads/2021/09/Empanadas-fritas-o-al-horno.jpg", categories: [], isAvailable: true },
         { name: "agua", price: 100, sales: 99, description: "aguita refrescante :D", img: "https://www.cucinare.tv/wp-content/uploads/2021/09/Empanadas-fritas-o-al-horno.jpg", categories: [], isAvailable: true },
@@ -51,10 +64,10 @@ const Dashboard = () => {
     return (
         <>
             <BackgroundImage src={src} imageLoader={imageLoader} />
-            <div className="fixed inset-0">
+            <div className="fixed inset-0 scale-100">
                 <NavBar />
-                <ProductCatalog productList={dummyProducts} />
-                <AboutProduct />
+                <ProductCatalog productList={dummyProducts} handleProductClick={handleProductClick} />
+                <ProductStatistics selectedProduct={selectedProduct} />
             </div>
         </>
     )
@@ -83,7 +96,9 @@ function BackgroundImage({ src, imageLoader }: { src: string, imageLoader: boole
 
 
 
-const ProductCatalog = ({ productList }: { productList: products[] }) => {
+const ProductCatalog = ({ productList, handleProductClick }: { productList: product[],  handleProductClick: (product: product) => void  }) => {
+
+    const [selectedProduct, setSelectedProduct] = useState<product | null>(null);
 
     return (
         <div>
@@ -91,12 +106,12 @@ const ProductCatalog = ({ productList }: { productList: products[] }) => {
                 <div>
                     <div>
                         <div className="flex flex-col items-center justify-center mt-24 ">
-                            <h1 className="text-3xl font-bold text-customRed">Catalogo de Productos</h1>
+                            <h1 className="text-3xl font-bold text-customRed">Catálogo de Productos</h1>
                             <hr className="bg-customPink h-1 w-72 mt-2" />
 
-                            <div className=' overflow-y-auto border border- mt-3 w-80 h-[80vh]'>
+                            <div className=' overflow-y-auto mt-3 w-80 h-[80vh]'>
                                 {productList.map((product, index) => (
-                                    <div>
+                                    <div key={index} onClick={() => handleProductClick(product)}>
                                         <ProductThumbnail product={product} />
                                     </div>
                                 ))}
@@ -112,25 +127,55 @@ const ProductCatalog = ({ productList }: { productList: products[] }) => {
     )
 }
 
-const ProductThumbnail = ({ product }: { product: products }) => {
+const ProductThumbnail = ({ product }: { product: product }) => {
+
+    const displayedCategories = product.categories.slice(0, 3);
 
 
     return (
-        <div className='bg-white rounded-lg mt-5 h-24 w-72'>
-            <div className='flex'>
-                <img src={product.img} alt='' className='w-24 h-24 object-cover rounded-lg' />
-                <h1>{product.name}</h1>
+        <>
+            <div className='bg-white rounded-lg mt-5 h-24 w-72 hover:scale-105 ease-in-out duration-200'>
+                <div className='flex'>
+                    <img src={product.img} alt='' className='w-16 h-24 object-cover rounded-lg' />
+                    <div className='flex-col ml-2 mt-3 text-sm'>
+                        <h1 className='font-bold'>
+                            {product.name.length > 25 ? product.name.substring(0, 25) + '...' : product.name}
+                        </h1>
+                        <hr className="bg-customPink h-1 w-48 rounded-lg" />
+                        <h1 className='font-bold mt-1'>Precio: ${product.price}</h1>
+                        <div className='flex justify-normal gap-2 w-52  overflow-x-scroll'>
+                            {displayedCategories.map((category, index) => (
+                                <div key={index}>
+                                    <CategoryTag category={category} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
+        </>
+    )
+
+}
+
+const CategoryTag = ({ category }: { category: category }) => {
+
+    return (
+        <div className='h-5 w-full pl-2 pr-2 rounded-xl text-center mt-1 bg-customRed text-white font-bold'>
+            {category.title}
+
         </div>
     )
 
 }
 
-const AboutProduct = () => {
+const ProductStatistics = ({ selectedProduct }: { selectedProduct: product | null }) => {
     return (
         <div className=' absolute mt-20 right-80 rounded-3xl h-[85vh] w-[120vh] bg-customBeige ml-5'>
             <div>
-
+                {selectedProduct?.name}
+                {selectedProduct?.description}
+                {selectedProduct?.price}
             </div>
         </div>
     )

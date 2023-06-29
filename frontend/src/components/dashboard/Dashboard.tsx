c6eb0ad6fdfd8df3747175cc0d2592729071faed
santi <santisevitz@hotmail.com>
2023-06-29 18:13:58 -0300

    agrego de filter bar

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -46,7 +46,7 @@ const Dashboard = () => {
         img.src = src
     }, [src])
 
-    const handleProductClick = (product: product) => {
+    const handleProductClick = (product: product | null) => {
         setSelectedProduct(product);
     }
 
@@ -90,6 +90,9 @@ function BackgroundImage({ src, imageLoader }: { src: string, imageLoader: boole
 
 const ProductCatalog = ({ productList, handleProductClick }: { productList: product[], handleProductClick: (product: product) => void }) => {
 
+
+    const [filterBy, setfilterBy] = useState("");
+
     return (
         <div>
             <div className="absolute bg-customBeige rounded-r-3xl  w-96 h-screen  ">
@@ -97,18 +100,18 @@ const ProductCatalog = ({ productList, handleProductClick }: { productList: prod
                     <div>
                         <div className="flex flex-col items-center justify-center mt-24 ">
                             <h1 className="text-3xl font-bold text-customRed">Catálogo de Productos</h1>
-                            <hr className="bg-customPink h-1 w-72 mt-2" />
+                            <hr className="bg-customPink h-1 w-72 my-2" />
 
-                            <div className=' overflow-y-auto mt-3 w-80 h-[80vh]'>
-                                {productList.map((product, index) => (
-                                    <div key={index} onClick={() => handleProductClick(product)}>
-                                        <ProductThumbnail product={product} />
-                                    </div>
-                                ))}
-
-
+                            <div className='w-80'>
+                                <ShowProducts filterState={[filterBy, setfilterBy]} handleProductClick={handleProductClick} />
+                                <div className='overflow-y-auto h-[80vh] '>
+                                    {productList.map((product, index) => (
+                                        <div key={index} onClick={() => handleProductClick(product)}>
+                                            <ProductThumbnail product={product} />
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
@@ -116,6 +119,57 @@ const ProductCatalog = ({ productList, handleProductClick }: { productList: prod
         </div>
     )
 }
+
+
+const ShowProducts = ({ filterState, handleProductClick }: { filterState: [string, React.Dispatch<React.SetStateAction<string>>], handleProductClick: (product: product|null) => void }) => {
+
+    const [filterBy, setfilterBy] = filterState;
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const allProductos = "Mostrar todos los productos"
+
+
+    const showAllProdcuts = () => {
+        setfilterBy(allProductos)
+        handleProductClick(null)
+        setIsOpen(false)
+    }
+
+    const categories = [
+        "snacks", "empanadas", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza"
+    ]
+
+    return (
+        <div className="relative">
+            <button onClick={() => setIsOpen((prev) => (!prev))}
+                id="dropdownDefaultButton"
+                data-dropdown-toggle="dropdown"
+                className="text-white bg-customRed hover:bg-customDarkRed  font-bold text-xl rounded-full flex justify-center py-1 w-full text-center transition-all"
+                type="button">
+                {filterBy ? filterBy : 'Buscar por categoria'}
+            </button>
+
+            {isOpen && <div className='overflow-y-auto scroll- absolute z-10 my-1 w-full h-44 bg-customRed opacity-95 mt-2 rounded-3xl font-bold text-white text-lg transition-all'>
+                <div className='flex flex-col text-center justify-center'>
+                    <div onClick={() => showAllProdcuts()} className='my-2 mx-3 hover:bg-customDarkRed rounded-3xl transition-all'>
+                        {allProductos}
+                    </div>
+                    <hr className="bg-white h-1  ml-3 mr-1 rounded-xl" />
+
+                    {categories.map((category, index) => (
+                        <div className='my-1 mx-3 hover:bg-customDarkRed rounded-3xl transition-all' key={index} onClick={() => setfilterBy(category)}>
+                            {category}
+                        </div>
+                    ))}
+
+                </div>
+            </div>}
+
+        </div>
+    );
+};
+
 
 
 const ProductThumbnail = ({ product }: { product: product }) => {
@@ -126,7 +180,7 @@ const ProductThumbnail = ({ product }: { product: product }) => {
 
     return (
         <>
-            <div className='bg-white rounded-lg mt-5 h-24 w-72 hover:scale-105 ease-in-out duration-200'>
+            <div className='bg-white rounded-lg mt-5 ml-2 h-24 w-11/12 hover:scale-105 ease-in-out duration-200'>
                 <div className='flex'>
                     <img src={product.img} alt='' className='w-16 h-24 object-cover rounded-lg' />
                     <div className='flex-col ml-2 mt-3 text-sm'>
@@ -190,7 +244,7 @@ const ProductStatistics = ({ selectedProduct }: { selectedProduct: product | nul
     const [customEndDate, setCustomEndDate] = useState<Date>();
 
     return (
-        <div className="right-1 flex justify-center mt-20 md:right-80 rounded-3xl bg-customBeige ml-5">
+        <div className="right-1 flex justify-center mt-20 md:right-80 rounded-3xl bg-customBeige ml-5 overflow-hidden">
             <div className="md:w-[95%] m-5 border-2 border-customPink rounded-3xl">
                 <div className="relative top-4">
                     <div className="ml-3 text-customRed font-bold">
@@ -211,7 +265,7 @@ const ProductStatistics = ({ selectedProduct }: { selectedProduct: product | nul
                                 customStartDate={customStartDate}
                                 customEndDate={customEndDate}
                             />
-                            <div className="md:flex-row  flex gap-7 justify-center flex-shrink-0 flex-wrap">
+                            <div className="md:flex-row flex justify-center flex-shrink-0 flex-wrap gap-7">
                                 <FilterOptions setFilterOption={setFilterOption} />
                                 <RangeDatePicker
                                     customStartDate={customStartDate}
@@ -220,10 +274,9 @@ const ProductStatistics = ({ selectedProduct }: { selectedProduct: product | nul
                                     setCustomEndDate={setCustomEndDate}
                                 />
                             </div>
-                            <div className="-mt-16 relative grid grid-cols-1 md:grid-cols-2 gap-8">
-                                <PieChartStatistics selectedProduct={selectedProduct} />
-                                <ShowProductImage selectedProduct={selectedProduct} />
-                            </div>
+
+                            <PieChartStatistics selectedProduct={selectedProduct} />
+                            <ShowProductImage selectedProduct={selectedProduct} />
                         </>
                     )}
                 </div>
@@ -231,7 +284,7 @@ const ProductStatistics = ({ selectedProduct }: { selectedProduct: product | nul
         </div>
     );
 };
-  
+
 
 
 const FilterOptions = ({ setFilterOption }: { setFilterOption: React.Dispatch<React.SetStateAction<string | undefined>> }) => {

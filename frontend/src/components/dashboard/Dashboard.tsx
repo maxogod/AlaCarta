e7f94c0517805883329167d4ffb23d7a798dc61d
santi <santisevitz@hotmail.com>
2023-07-03 22:03:19 -0300

    spacing lol

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -11,8 +11,9 @@ const Dashboard = () => {
 
     const src = "https://toohotel.com/wp-content/uploads/2022/09/TOO_restaurant_Panoramique_vue_Paris_Seine_Tour_Eiffel_2.jpg"
     const [imageLoader, setImageLoader] = useState(false)
-
     const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+    
 
 
     useEffect(() => {

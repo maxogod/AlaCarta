ba50b2d7e7d7ca79168a67724c28efa9b3ae627c
maxogod <maxi18wars@gmail.com>
2023-07-09 19:34:40 -0300

    Edit Menu Donzo

--- frontend/src/components/menu/Banner.tsx
+++ frontend/src/components/menu/Banner.tsx
@@ -1,11 +1,14 @@
+import { ReactNode } from "react"
 
-const Banner = () => {
+const Banner = ({ children, picture }: { children: ReactNode, picture: string | undefined }) => {
     return (
-        <img
-            className="w-screen h-36 aspect-w-16 aspect-h-9 object-cover rounded-t-3xl drop-shadow-3xl"
-            src="https://toohotel.com/wp-content/uploads/2022/09/TOO_restaurant_Panoramique_vue_Paris_Seine_Tour_Eiffel_2.jpg"
-            alt="Restaurant Image"
-        />
+        <div className="relative">
+            <img
+                className="w-screen h-36 aspect-w-16 aspect-h-9 object-cover rounded-t-3xl drop-shadow-3xl"
+                src={picture}
+                alt="Restaurant Image" />
+            {children}
+        </div>
     )
 }
 
--- frontend/src/components/menu/Menu.tsx
+++ frontend/src/components/menu/Menu.tsx
@@ -6,7 +6,9 @@ import Banner from "./Banner";
 import Categories from "./CategoryBar";
 import OrderCart from "./OrderCart";
 import ProductList from "./ProductList";
+import EditMenu from "./EditMenu";
 import { useEffect, useState } from "react";
+import { FaEdit } from "react-icons/fa";
 
 
 const Menu = () => {
@@ -17,6 +19,7 @@ const Menu = () => {
     const { restaurant, isLoading } = useGetRestaurant()
     const [deployCategory, setDeployCategory] = useState<boolean>(window.innerWidth > 720)
     const [deployCart, setDeployCart] = useState<boolean>(window.innerWidth > 720)
+    const [deployEdit, setDeployEdit] = useState<boolean>(false)
     const isInMobile = window.innerWidth < 400
     const [menuIsVisible, setMenuIsVisible] = useState<boolean>(true)
 
@@ -48,6 +51,10 @@ const Menu = () => {
             {
                 isRestaurantLoaded ?
                     <div className="fixed inset-0 flex py-5" style={{ background: restaurant!.menu?.color }}>
+                        {deployEdit && <EditMenu
+                            setDeploy={setDeployEdit}
+                            currentColor={restaurant!.menu?.color}
+                            currentPicture={restaurant!.menu?.banner} />}
                         {
                             !deployCategory && (!isInMobile || (isInMobile && !deployCart)) &&
                             <div className="h-full absolute left-0 mt-3 z-10 flex sm:items-center ml-2">
@@ -67,7 +74,13 @@ const Menu = () => {
                         {
                             menuIsVisible &&
                             <div className="flex flex-col px-5">
-                                <Banner />
+                                <Banner picture={restaurant!.menu?.banner}>
+                                    <button
+                                        className="absolute right-2 bottom-4 hover:scale-125 duration-200 text-white shadow-2xl text-4xl"
+                                        onClick={() => { setDeployEdit(true) }}>
+                                        <FaEdit />
+                                    </button>
+                                </Banner>
                                 <ProductList category={selectedCategory} kPopular={kPopular} />
                             </div>
                         }

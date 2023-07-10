5ac5e54e43635605c27520327b2cdba8f9cb20b1
maxogod <maxi18wars@gmail.com>
2023-07-10 00:58:01 -0300

    orders loading spinner

--- frontend/src/components/orders/ShowOrders.tsx
+++ frontend/src/components/orders/ShowOrders.tsx
@@ -3,17 +3,19 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { OrderType } from '../../@types/stateTypes';
 import OrderThumbnail from './OrderThumbnail';
+import { IoRestaurantSharp } from 'react-icons/io5';
 
 const ShowOrders = ({ orderStatus }: { orderStatus: number }) => {
 
 
     const { restaurantUrl } = useParams()
-
+    const [ordersLoaded, setOrdersLoaded] = useState(false)
     const [orders, setOrders] = useState<OrderType[] | null>();
 
     const [updateOrders, setUpdateOrders] = useState<boolean>(true);
 
     useEffect(() => {
+        setOrdersLoaded(false)
         const fetchOrders = async () => {
             try {
                 const res = await axios.get(
@@ -24,6 +26,7 @@ const ShowOrders = ({ orderStatus }: { orderStatus: number }) => {
                 );
                 if (res.status === 404) return
                 setOrders(res.data)
+                setOrdersLoaded(true)
             } catch (err) {
                 console.log(err);
 
@@ -34,14 +37,22 @@ const ShowOrders = ({ orderStatus }: { orderStatus: number }) => {
     }, [updateOrders]);
 
     return (
-        <div className=' flex flex-col overflow-y-auto h-5/6 items-center gap-2 mt-3 '>
-            {orders?.map((order, index) => (
-                <div key={index}>
-                    <OrderThumbnail order={order} setUpdateOrders={setUpdateOrders} />
-                </div>
-            ))}
-
-        </div>
+        <>
+            {
+                ordersLoaded ?
+                    <div className=' flex flex-col overflow-y-auto h-5/6 items-center gap-2 mt-3 '>
+                        {orders?.map((order, index) => (
+                            <div key={index}>
+                                <OrderThumbnail order={order} setUpdateOrders={setUpdateOrders} />
+                            </div>
+                        ))}
+                    </div>
+                    :
+                    <div className='flex animate-pulse items-center justify-center h-full'>
+                        <IoRestaurantSharp className="text-9xl animate-spin text-customRed" />
+                    </div>
+            }
+        </>
     );
 };
 
